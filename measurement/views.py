265a20e6ce6ae6ed2f8b532e3e8fed426849c7c0
artem-chizhov <113351797+artem-chizhov@users.noreply.github.com>
2023-05-03 12:26:33 +0300

    Add files via upload

---
+++ measurement/views.py
@@ -0,0 +1,38 @@
+# опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
+# ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
+from .models import Sensor
+from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
+
+
+class SensorListCreateView(ListCreateAPIView):
+    def get(self, request):
+        queryset = Sensor.objects.all()
+        serializer = SensorSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SensorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SensorRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+
+class MeasurementListCreateView(ListCreateAPIView):
+    def post(self, request):
+        serializer = MeasurementSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
